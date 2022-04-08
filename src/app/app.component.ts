81f2d1e5738c9036f31ed75e430e003384e89263
Bhaskara-Poloju <polojubhaskarachary@gmail.com>
2022-04-08 09:52:54 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_Services/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'DatingApp';
+  users: any;
+
+  constructor(public http: HttpClient, private accountService: AccountService) {
+
+  }
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }  
+
+  setCurrentUser() {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    this.accountService.setCurrentUser(user);
+  }
+}
